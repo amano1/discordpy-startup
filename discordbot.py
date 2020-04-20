2186c786ef7ad9ad1f961fb5ff82ea6ee351fb0a
amano1 <64009823+amano1@users.noreply.github.com>
2020-04-21 00:40:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -56,7 +56,11 @@ async def on_ready():
             else:
                 print(f"⭕┃{member}のニックネームに║Point:0を追加")
     else:
-        user_dic = sorted(user_dic.items(), key=lambda x:x[1])
+        user_list = sorted(user_dic.items(), key=lambda x:x[1])
+        user_dict = None
+        for set in user_list:
+            for key,value in zip(set[0],set[1]):
+                user_dict[key] = value
         print(user_dic)
         ch_1 = client.get_channel(701803530566238290)
         ch_2 = client.get_channel(701803756571983893)
