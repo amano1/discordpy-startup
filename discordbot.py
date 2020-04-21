fbe354a5ecd2f3d6c86cd1c76f7adf2193c7cf31
amano1 <64009823+amano1@users.noreply.github.com>
2020-04-21 12:33:34 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -163,8 +163,12 @@ async def on_message(message):
         else:
             user_dic[user.id] = 1
         member = message.guild.get_member(user.id)
-        await member.edit(nick = f"［{user_dic[user.id]}］{member.name}")
-        
+        try:
+            await member.edit(nick = f"［{user_dic[user.id]}］{member.name}")
+        except:
+            pass
+        else:
+            pass
     if message.content == "i)reward":
         ch_id = 701721786592657461
         ch = client.get_channel(ch_id)
