91afc5797ab1a29bbd27440a1efb30b79747b75a
amano1 <64009823+amano1@users.noreply.github.com>
2020-04-26 17:03:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -52,7 +52,29 @@ async def on_ready():
     num = len(guild.members)
     await client.change_presence(activity=discord.Game(name=f"{num}members in this server"))
     loop.start()
-        
+    
+@client.event
+async def on_member_join(member): 
+    embed = discord.Embed(
+        title = "Amano's Macro Serverへようこそ！！",
+        description = "まずは[ホームページ](https://tsukumoshimo.wixsite.com/amsserver)を確認してね！^w^)9",
+        color = discord.Color.green())
+    await member.send(embed = embed)
+    ch = client.get_channel(681211296297123956)
+    ch_2 = client.get_channel(674983698080202797)
+    await ch.send(f"{member.mention}が入船しました。現在錨泊中です。")
+    await ch_2.send(f"{member.mention}が入船しました。現在錨泊中です。")
+    embed = discord.Embed(
+        title = "招待状が届きました!!",
+        description = "[🎫](https://discord.gg/PeV2tek)←クリック")
+    await member.send(embed = embed)
+    
+@client.event
+async def on_member_remove(member): 
+    ch = client.get_channel(681211296297123956)
+    ch_2 = client.get_channel(674983698080202797)
+    await ch.send(f"{member.mention}がAMSを去りました、( ´Д｀)ﾉ~ﾊﾞｲﾊﾞｲ")
+    await ch_2.send(f"{member.mention}がAMSを去りました、( ´Д｀)ﾉ~ﾊﾞｲﾊﾞｲ")
 @tasks.loop(seconds=10)
 async def loop():
     global user_dic
@@ -253,27 +275,6 @@ async def on_message(message):
         delech=None
         deleuser=None
 
-@client.event:
-async def on_member_join(member): 
-    embed = discord.Embed(
-        title = "Amano's Macro Serverへようこそ！！",
-        description = "まずは[ホームページ](https://tsukumoshimo.wixsite.com/amsserver)を確認してね！^w^)9",
-        color = discord.Color.green())
-    await member.send(embed = embed)
-    ch = client.get_channel(681211296297123956)
-    ch_2 = client.get_channel(674983698080202797)
-    await ch.send(f"{member.mention}が入船しました。現在錨泊中です。")
-    await ch_2.send(f"{member.mention}が入船しました。現在錨泊中です。")
-    embed = discord.Embed(
-        title = "招待状が届きました!!",
-        description = "[🎫](https://discord.gg/PeV2tek)←クリック")
-    await member.send(embed = embed)
-    
-@client.event:
-async def on_member_remove(member): 
-    ch = client.get_channel(681211296297123956)
-    ch_2 = client.get_channel(674983698080202797)
-    await ch.send(f"{member.mention}がAMSを去りました、( ´Д｀)ﾉ~ﾊﾞｲﾊﾞｲ")
-    await ch_2.send(f"{member.mention}がAMSを去りました、( ´Д｀)ﾉ~ﾊﾞｲﾊﾞｲ")
+
     
 client.run(TOKEN)
